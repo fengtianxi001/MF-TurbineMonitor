7d92cac9265cba36f952bba92728d7e5ffc50873
肛肠科冯主任 <625362207@qq.com>
2022-05-01 16:42:03 +0800

    Perf: 移除pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 import "styles/global.scss";
 import App from "./App.vue";
 import "./websocket/index";
-createApp(App).use(createPinia()).mount("#app");
+createApp(App).mount("#app");
