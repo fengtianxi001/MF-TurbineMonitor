37a514c41acb06487d88dac4c49f4e689dbc8b5f
肛肠科冯主任 <625362207@qq.com>
2022-09-17 20:43:49 +0800

    Update: TurbineMonitor

--- src/components/DashBoard/TurbineMonitor/index.vue
+++ src/components/DashBoard/TurbineMonitor/index.vue
@@ -1,28 +1,14 @@
 <template>
-  <BaseLoading v-show="!loading"> Loading: {{ Number(percent).toFixed(2) }} % </BaseLoading>
-  <div class="custom-three" ref="threeRef"></div>
+  <div class="turbine-monitor" ref="threeRef"></div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
 import { useTurbine } from '@/hooks/useTurbine'
 const threeRef = ref()
-const { loading, percent } = useTurbine(threeRef)
+useTurbine(threeRef)
 </script>
 <style>
-.custom-three {
+.turbine-monitor {
   width: 100vw;
   height: 100vh;
-}
-.loading {
-  position: absolute;
-  z-index: 9999;
-  font-size: 40px;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: rgba(0, 0, 0, 0.4);
-  color: #fff;
 }
 </style>
